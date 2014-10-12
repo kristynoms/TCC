2d59f52a2fe05d6200676379986e6285a1728f94
Kristy <kristy.noms@live.com>
2014-10-12 13:43:09 -0300

    inicio do design

---
+++ DeCoRes/menu/menu.php
@@ -0,0 +1,7 @@
+<div class="navbar-collapse collapse navbar-inverse-collapse">
+    <ul class="nav navbar-nav">
+        <li> <a class="navbar-brand" href="home.php">Home </a> </li>
+        <li> <a href="twitter.php"> Twitter </a> </li>
+        <li> <a href="face.php">Facebook </a> </li>
+    </ul>
+</div>
